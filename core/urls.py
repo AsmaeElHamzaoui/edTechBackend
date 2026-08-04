97d9dcb392f43b4d6acb33dde6e33ca870f0e9aa
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2026-08-04 10:00:00 +0100

    Initial commit: project configuration and base settings

---
+++ core/urls.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+
+    path(
+        "admin/",
+        admin.site.urls
+    ),
+
+    path(
+        "api/auth/",
+        include("users.urls")
+    ),
+
+    path(
+        "api/documents/",
+        include("documents.urls")
+    ),
+
+      path(
+        "api/chat/",
+        include("chat.urls")
+    ),
+
+]
